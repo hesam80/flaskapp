4ada01d30c9509c4b2d4b9cecdefc7bb63434ea7
hesam80 <hm.hoseyni@gmail.com>
2021-10-01 18:22:29 +0000

    app update

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import os, requests
 import random
-from flask import Flask, flash, redirect, render_template, request, url_for
+from flask import Flask, flash, redirect, render_template, request, url_for, abort
 
 
 app = Flask(__name__)
@@ -9,15 +9,28 @@ app = Flask(__name__)
 @app.route('/')
 def view():
     """Return a friendly HTTP greeting."""
-    message = "It's redeployedd Wow how it's Beautiful!"
+    firstmessage = "It's redeployedd Wow how it's Beautiful!"
     congramessage="Congratulations, you successfully deployed a container image to Cloud Run!"
     """Get Cloud Run environment variables."""
     service = os.environ.get('K_SERVICE', 'Unknown service')
     revision = os.environ.get('K_REVISION', 'Unknown revision')
     num=random.randint(1,12)
     hitt= num*2
-    return render_template('index.html',num=num,hit=hitt,congmsg=congramessage,Service=service,Revision=revision,message=message)
+    return render_template('index.html',num=num,hit=hitt,congmsg=congramessage,Service=service,Revision=revision,firstmsg=firstmessage)
 
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'POST':
+        if valid_login(request.form['username'],
+                       request.form['password']):
+            return log_the_user_in(request.form['username'])
+        else:
+            error = 'Invalid username/password'
+    # the code below is executed if the request method
+    # was GET or the credentials were invalid
+    return render_template('login.html', error=error)
 
 @app.route("/indexx")
 def indexx():
